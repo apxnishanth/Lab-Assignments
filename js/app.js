3d5d4d9a167be034b4efda2c42316e0dd27a408a
Chandra Kolluri <kolluc@amazon.com>
2019-02-22 14:09:01 -0800

    Initial Commit with required files

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 'user-strict';
 
 var username = prompt('Why dont you start with telling me your name?');
-console.log('Visitor Name: ', username);
+consoleMessage(username);
 
 var myName = prompt('Howdy..!' + username + ' Let me start with a simple question.What\'s my Name?');
-console.log('Visitor answered: ' ,myName);
+consoleMessage(myName);
 
 if (myName.toUpperCase() === 'NISHANTH'){
   alert('Awesome!..' + username + '. You had it right..');
@@ -16,9 +16,9 @@ var cities = ['Boston','Seattle','New York','California'];
 
 for (var i = 0; i < 3; i++){
   var myCity = prompt ('Which city do I live? Choose from ' + cities );
-  console.log('Visitor answered :',myCity);
+  consoleMessage(myCity);
   if (myCity.toUpperCase() === 'SEATTLE' ){
-    alert('No Way.. You are too awesome.You guessed it correct.');
+    alertMessage();
     break;
   }
   else {
@@ -27,50 +27,61 @@ for (var i = 0; i < 3; i++){
 }
 
 var myBirthdayYear = prompt ('Was I born in 1999?');
-console.log('Visitor answered :',myBirthdayYear);
+consoleMessage(myBirthdayYear);
 
 if (myBirthdayYear.toUpperCase() === 'YES' || myBirthdayYear.toUpperCase() === 'Y' ) {
   alert('No way... I am not that old.. I was born in 1986');
 } else {
-  alert('Kudos!!You guessed it right!!');
+  alertMessage();
 }
 
 var myFavoriteCusine = prompt('Is my favorite cusine Italian?');
-console.log('Visitor answered :',myFavoriteCusine);
+consoleMessage(myFavoriteCusine);
 
 if (myFavoriteCusine.toUpperCase() === 'YES' || myFavoriteCusine.toUpperCase() === 'Y' ) {
-  alert('Thats true..! I love Italian cuisine');
+  alertMessage();
 } else {
   alert('Nah..You are wrong.Better luck next time!');
 }
 
 for (var j = 0; j <= 6; j++){
   var myFavoriteNumber = prompt('What is my favorite number ?');
-  console.log('Visitor answered :',myFavoriteNumber);
-  if (myFavoriteNumber === 6){
-    alert('You guessed it right');
+  consoleMessage(myFavoriteNumber);
+  if (myFavoriteNumber === '6'){
+    alertMessage();
     break;
   }
-  while (myFavoriteNumber <= 5) {
-    alert('Thats lower than my favorite.Let me give you one more try')
+  while (myFavoriteNumber <= '5') {
+    alert('Thats lower than my favorite.Let me give you one more try');
     break;
   }
-  if (myFavoriteNumber > 6){
+  if (myFavoriteNumber > '6'){
     alert('Thats higher than my favorite.Let me give you one more try');
   }
 }
 
+
 var favoriteMoviesArray = ['Usual Suspects','The Dark Knght','The Shining','The Wailing','Avatar'];
 
-
+var flag = false;
 for (var z = 0; z <= 3; z++) {
   var myFavoriteMovie = prompt('Could you guess my favorite movie?');
-  console.log('Visitor answered :',myFavoriteMovie);
+  consoleMessage(myFavoriteMovie);
   for (var k = 0; k <= 4; k++){
     if (favoriteMoviesArray[k] === myFavoriteMovie){
-      alert('You guessed it right..!');
+      alertMessage();
+      flag = true;
       break;
     }
+    if (flag === true) { break;}
   }
+}
+
+function alertMessage() {
+  alert('You guessed it right..!');
+}
+
+function consoleMessage(userResponse) {
+  console.log('Visitor answered :',userResponse);
 }
 alert('Thanks for playing the game! Good luck!');
